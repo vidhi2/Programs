b54e3309c3ee2d21734c1c4d9ddc010d7ff613d6
Lovekesh garg <iamvoid13@gmail.com>
2014-11-16 19:54:51 +0530

    initial commit

---
+++ factorial.cpp
@@ -0,0 +1,42 @@
+/*
+	A simple C++ program to calculate n!.
+	Both by :
+		recursion and non_recursion(loops)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+ull fac_rec(int n);
+ull fac_not_rec(int n);
+
+int main(){
+	int t,i;
+	cout<<"How many numbers do you have : ";
+	cin>>t;
+	cout<<"Now enter "<<t<<" numbers :"<<endl;
+	for(i=0;i<t;i++){
+		int n;
+		cout<<"--Enter your number : ";
+		cin>>n;
+		cout<<"The value of"<<n<<"! is "<<endl;
+		cout<<" By recursion :"<<fac_rec(n)<<endl;
+		cout<<" By non-recursion :"<<fac_not_rec(n)<<endl;
+	}
+	return 0;
+}
+ull fac_rec(int n){
+	if(n<2)
+		return 1;
+	return n*fac_rec(n-1);
+}
+ull fac_not_rec(int n){
+	ull ans=1;
+	int i;
+	for(i=1;i<=n;i++)
+		ans=ans*i;
+	return ans;
+}
